fb00ddedad9caa4c2518c7c94478b9caab85b663
Arslaan Asad <73607166+W44@users.noreply.github.com>
2025-06-04 17:08:36 +0300

    Initial Commit

---
+++ src/main/java/com/crmw/CRM_BE/CrmBeApplication.java
@@ -0,0 +1,13 @@
+package com.crmw.CRM_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrmBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crmw/CRM_BE/CrmBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crmw.CRM_BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
